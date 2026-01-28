a0efcf5f66dc783706749630a993ae13da374a90
cyanyaan <ankitsarkar431@gmail.com>
2026-01-28 19:44:21 +0530

    fix

--- com/craftinginterpreters/lox/Lox.java
+++ com/craftinginterpreters/lox/Lox.java
@@ -7,8 +7,6 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;
-
 
 public class Lox{
     static boolean hadError = false;
@@ -22,47 +20,47 @@ public class Lox{
             runPrompt();
         }
     }
-}
 
-private static void runFile(String path) throws  IOException{
-    byte[] bytes = Files.readAllBytes(Paths.get(path));
-    run(new String(bytes, Charset.defaultCharset()));
+    private static void runFile(String path) throws  IOException{
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
 
-    if(hadError) {
-        System.exit(65);
+        if(hadError) {
+            System.exit(65);
+        }
     }
-}
 
 
 
-private static void runPrompt() throws IOException{
-    InputStreamReader input = new InputStreamReader(System.in);
-    BufferedReader reader = new BufferedReader(input);
+    private static void runPrompt() throws IOException{
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
 
-    for(;;){
-        System.out.println("> ");
-        String line = reader.readLine();
-        if(line == null) break;
-        run(line);
-        hadError = false;
+        for(;;){
+            System.out.println("> ");
+            String line = reader.readLine();
+            if(line == null) break;
+            run(line);
+            hadError = false;
+        }
     }
-}
 
-private static void run(String source){
-    Scanner scanner = new Scanner(source);
-    List<Token> tokens = scanner.scanTokens();
+    private static void run(String source){
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
 
-    for (token : tokens)
-    {
-        System.out.println(token);
+        for (Token token : tokens)
+        {
+            System.out.println(token);
+        }
     }
-}
 
-static void error(int line, String message) {
-    report(line, "", message);
-}
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
 
-private static void report(int line, String where, String message){
-    System.err.println("[Line " + line + "] Error " + where + ": " + message );
-    hadError = true;
+    private static void report(int line, String where, String message){
+        System.err.println("[Line " + line + "] Error " + where + ": " + message );
+        hadError = true;
+    }
 }
